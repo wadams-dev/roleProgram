bddc2658620a4160cdd21155516556ae88084cf9
wadams-dev <50535899+wadams-dev@users.noreply.github.com>
2020-03-24 15:10:49 -0400

    Add initial program files

---
+++ script.js
@@ -0,0 +1,18 @@
+ var classMates = ['Alejandro', 'Ben', 'Cristian', 'Daniel', 'David', 'Derek', 'Esau',
+                    'Fatima', 'Ja\'Ron', 'Rob', 'Jose','Josh', 'Flores', 'Julian',
+                     'Kelly', 'Kelvin', 'Kyle', 'Leo', 'Lenise', 'Maya', 'Nathan',
+                     'Vanessa', 'Vlad', 'Will', 'Wilson'];
+
+ var speaker;
+ var counter = 1;
+
+function getSpeaker(){
+    speaker = Math.floor((Math.random() * classMates.length));
+    return speaker;
+}
+
+for(var i = 0; i < 25; i++){
+    console.log(counter + ". " + classMates[getSpeaker()]);
+    classMates.splice(speaker,1);
+    counter++;
+}
